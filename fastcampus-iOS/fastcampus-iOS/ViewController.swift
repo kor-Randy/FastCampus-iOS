541dd2cf8cd661fae26d9178f094f46fe0316d20
kor-Randy <randy_95@naver.com>
2020-08-02 22:14:31 +0900

    2강 완료 Closure, Local/Instance Variable, 타입추론, 메소드, 오토레이아웃 ...

--- fastcampus-iOS/fastcampus-iOS/ViewController.swift
+++ fastcampus-iOS/fastcampus-iOS/ViewController.swift
@@ -10,19 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue = 0
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setPrice()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func hello(_ sender: Any) {
         
-        let alert = UIAlertController(title: "Hello", message: "My First App!!", preferredStyle: .alert)
+        let message = "가격은 ₩\(currentValue)입니다."
         
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default,
+                                   handler: { action in
+                                    self.setPrice()
+        })
         alert.addAction(action)
         present(alert,animated: true,completion: nil)
-        
+       
+    }
+    
+    func setPrice(){
+        let randomPrice = arc4random_uniform(10000) + 1 // 랜덤
+        currentValue = Int(randomPrice)
+        priceLabel.text = "`\(currentValue)"
     }
     
 }
