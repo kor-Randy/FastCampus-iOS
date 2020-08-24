b37f9bfb4ede6ace042fb6f425e3d1b5244a6bab
kor-Randy <randy_95@naver.com>
2020-08-24 14:03:45 +0900

    test

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by 지현우 on 2020/08/24.
+//  Copyright © 2020 wlgusdn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
